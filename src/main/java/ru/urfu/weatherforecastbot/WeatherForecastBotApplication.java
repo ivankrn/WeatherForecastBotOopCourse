ca7c5fd8dd45a9de7705b9d500e236f286f890f8
Ivan Kropotin <kropotiniv@yandex.ru>
2023-11-01 12:37:00 +0500

    Initial commit

---
+++ src/main/java/ru/urfu/weatherforecastbot/WeatherForecastBotApplication.java
@@ -0,0 +1,21 @@
+package ru.urfu.weatherforecastbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Класс для точки входа в приложение
+ */
+@SpringBootApplication
+public class WeatherForecastBotApplication {
+
+	/**
+	 * Точка входа, запускающая приложение Spring
+	 *
+	 * @param args аргументы командной строки
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastBotApplication.class, args);
+	}
+
+}
