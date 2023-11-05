92c5ff245b8ab957cf5ef5f0221c725c485c8667
Ivan Kropotin <kropotiniv@yandex.ru>
2023-11-05 14:47:46 +0500

    Добавил исключения при неверном json на входе

--- src/main/java/ru/urfu/weatherforecastbot/service/WeatherForecastsDeserializer.java
+++ src/main/java/ru/urfu/weatherforecastbot/service/WeatherForecastsDeserializer.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 // TODO: 03.11.2023 Спросить, нужно ли делать интерфейс
 /**
@@ -18,6 +19,7 @@ import java.util.List;
 public class WeatherForecastsDeserializer {
 
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+    private final String exceptionMessage = "Wrong json provided: ";
 
     /**
      * Преобразует ответ сервера в список прогнозов погоды
@@ -25,21 +27,32 @@ public class WeatherForecastsDeserializer {
      * @param response ответ сервера
      * @return список прогнозов погоды
      */
-    public List<WeatherForecast> parseJsonResponseToWeatherForecasts(JsonNode response) {
+    public List<WeatherForecast> parseJsonResponseToWeatherForecasts(JsonNode response) throws IllegalArgumentException {
         JsonNode hourlyData = response.get("hourly");
+        if (hourlyData == null) {
+            throw new IllegalArgumentException(exceptionMessage + response.asText());
+        }
         JsonNode times = hourlyData.get("time");
         JsonNode temperatures = hourlyData.get("temperature_2m");
         JsonNode humidities = hourlyData.get("relativehumidity_2m");
         JsonNode feelsLikeTemperatures = hourlyData.get("apparent_temperature");
         JsonNode pressures = hourlyData.get("surface_pressure");
+        if (times == null || temperatures == null || humidities == null
+                || feelsLikeTemperatures == null || pressures == null
+                || times.size() != temperatures.size()
+                || times.size() != humidities.size()
+                || times.size() != feelsLikeTemperatures.size()
+                || times.size() != pressures.size()) {
+            throw new IllegalArgumentException(exceptionMessage + response.asText());
+        }
         List<WeatherForecast> forecasts = new ArrayList<>(times.size());
         for (int i = 0; i < times.size(); i++) {
             forecasts.add(new WeatherForecast(
                     LocalDateTime.parse(times.get(i).asText(), dateTimeFormatter),
                     temperatures.get(i).asDouble(),
                     feelsLikeTemperatures.get(i).asDouble(),
-                    PressureConverter.convertHpaToMmhg(pressures.get(i).asDouble()),
-                    humidities.get(i).asDouble())
+                    (int) PressureConverter.convertHpaToMmhg(pressures.get(i).asDouble()),
+                    humidities.get(i).asInt())
             );
         }
         return forecasts;
